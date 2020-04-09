12ac34db87546bd3e2616dce2cdbf07b4fa42d6a
dmitrypavlyuk <dmitrypavlyuk@4d325216-8dce-4ffd-a5a8-40fc6238e198>
2020-04-09 09:15:41 +0000

    minor
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fsmts@20 4d325216-8dce-4ffd-a5a8-40fc6238e198

--- pkg/R/fsCCF.R
+++ pkg/R/fsCCF.R
@@ -1,9 +1,10 @@
 fsCCF <- function(mts, max.lag,  show.progress = TRUE) {
   n <- ncol(mts)
   res <- matrix(0, n*max.lag, n)
+  mts<-stats::na.omit(dat)
   for (i in 1:n){
     for (j in i:n){
-        corVals <- stats::ccf(mts[,i],mts[,j], lag.max=max.lag, plot=F, na.action = stats::na.pass)
+        corVals <- stats::ccf(mts[,i],mts[,j], lag.max=max.lag, plot=F)
         res[j+(0:(max.lag-1))*n, i] <- rev(corVals$acf[1:max.lag])
         res[i+(0:(max.lag-1))*n, j] <- corVals$acf[(max.lag+2):(2*max.lag+1)]
     }
